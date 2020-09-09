cb910b43423e4977a2794efee5cca735387b2e3f
Samadmemon991 <Samadmemon991@users.noreply.github.com>
2020-09-09 08:06:18 +0500

    ver 1.1

--- app/src/main/java/com/example/madproject/WeaponSelection.java
+++ app/src/main/java/com/example/madproject/WeaponSelection.java
@@ -1,21 +1,23 @@
 package com.example.madproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -26,6 +28,7 @@ public class WeaponSelection extends AppCompatActivity implements View.OnClickLi
     Intent intent;
     View inflator;
     String selectedCategory;
+    ImageView weaponImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,6 +124,12 @@ public class WeaponSelection extends AppCompatActivity implements View.OnClickLi
             viewIds.add(inflator);
             weaponName = inflator.findViewById(R.id.text);
             weaponName.setText(d.getKey().toString());
+
+// image token
+         Picasso.get().load("https://bit.ly/3m5b6mI")
+                    .into(weaponImg);
+
+
             inflator.setOnClickListener(WeaponSelection.this);
 
         }
